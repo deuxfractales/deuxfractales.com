c826be023387a3172d08bd11056e6c77cd7bdd1f
knightofhonur12 <chowdhury.ma@outlook.com>
2020-05-09 20:56:40 -0400

    Finished Julia Set processing

--- p5/public/sketch.js
+++ p5/public/sketch.js
@@ -3,15 +3,28 @@ var n = 2
 var d = 14
 var k = n / d
 
-// let x = 0.1
-// let y = 0.3
-// let z= x;
-// let ca= -0.8;
-// let cb= 0.156;
-// let d= y;
+var minval = -0.5;
+var maxval = 0.5;
+
+var minSlider;
+var maxSlider;
+
+var x;
+var y;
+// var z= x;
+var ca= -0.8;
+var cb= 0.156;
+var frDiv;
+// var d= y;
 
 function setup() {
     createCanvas(400, 400);
+    pixelDensity(1);
+
+    // minSlider = createSlider(-2.5, 0, -2.5, 0.01);
+//   maxSlider = createSlider(0, 2.5, 2.5, 0.01);
+
+    frDiv = createDiv('');
     // socket = io.connect('http://localhost:3000')
     // socket.on('shape', newDrawing)
 }
@@ -40,29 +53,83 @@ function draw() {
     noFill();
     stroke(255);
     strokeWeight(1);
-    for (var a = 0; a < TWO_PI * d; a += 0.02) {
-        var r = 200 * cos(k * a);
-        var x = r * cos(a);
-        var y = r * sin(a);
-        vertex(x,y);
+
+    var maxiterations = 100;
+
+    loadPixels();
+    for (var x = 0; x < width; x++) {
+      for (var y = 0; y < height; y++) {
+        var a = map(x, 0, width, -2.5, 2.5);
+        var b = map(y, 0, height, -2.5, 2.5);
+  
+        var ca = -0.8;
+        var cb = 0.156;
+  
+        var n = 0;
+  
+        while (n < maxiterations) {
+          var aa = a * a - b * b;
+          var bb = 2 * a * b;
+          a = aa + ca;
+          b = bb + cb;
+          if (a * a + b * b > 16) {
+            break;
+          }
+          n++;
+        }
+  
+        var bright = map(n, 0, maxiterations, 0, 1);
+        bright = map(sqrt(bright), 0, 1, 0, 255);
+  
+        if (n == maxiterations) {
+          bright = 0;
+        }
+  
+        var pix = (x + y * width) * 4;
+        pixels[pix + 0] = bright;
+        pixels[pix + 1] = bright;
+        pixels[pix + 2] = bright;
+        pixels[pix + 3] = 255;
+      }
+    }
+    updatePixels();
+  
+    frDiv.html(floor(frameRate()));
+    // for (var a = 0; a < TWO_PI * d; a += 0.02) {
+    //     var r = 200 * cos(k * a);
+    //     var x = r * cos(a);
+    //     var y = r * sin(a);
+    //     vertex(x,y);
         // var data = {
         //     x: x,
         //     y: y
         // }
 
-    }
+    // }
 
-    // for(let a = 0; a < 50; a += 0.1) {
+    // for (var a = 0; a < TWO_PI * d; a += 0.02) {
+            // var r = 200 * cos(k * a);
+            // x = a*a+ ca;
+            // y = 2*a + cb;
+            // vertex(x ,y);
+            // var data = {
+            //     x: x,
+            //     y: y
+            // }
+    
+    // }
+
+    // for(var a = 0; a < 5; a += 0.1) {
     //   // let d= 0;
-    //   let z_new= z*z - d*d+ ca;
-    //   let b_new= 2*z*d + cb;
-    //   z= z_new;
-    //   d= b_new;
-    //   vertex(z_new,b_new)
-    //   let dataJ = {
+    //   var z_new= a*a - d*d;
+    //   var b_new= 2*a*d ;
+    // //   z= z_new;
+    // //   d= b_new;
+    //   vertex(z_new,b_new);
+    //   var dataJ = {
     //     x: z_new,
     //     y: b_new,
     //   };
     // }
-    endShape(close);
+    // endShape(close);
 }
